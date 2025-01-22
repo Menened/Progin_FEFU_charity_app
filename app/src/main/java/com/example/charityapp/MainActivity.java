cff81e9c87ab35c82b21ca082bf014f9111073d5
Menened <vos77vos@yandex.ru>
2025-01-22 20:40:04 +1000

    With nav panel

--- app/src/main/java/com/example/charityapp/MainActivity.java
+++ app/src/main/java/com/example/charityapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.charityapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,26 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        Button buttonNavigate = findViewById(R.id.Login);
+
+        // Устанавливаем обработчик нажатия на кнопку
+        buttonNavigate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Переход на следующую активность
+                navigateToNextActivity();
+            }
+        });
+    }
+
+    // Функция для перехода на следующую активность
+    private void navigateToNextActivity() {
+        // Создаём Intent для перехода на следующую активность
+        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+
+        // Запускаем следующую активность
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/charityapp/MainActivity2.java
@@ -0,0 +1,59 @@
+package com.example.charityapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.charityapp.databinding.ActivityMain2Binding;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    ActivityMain2Binding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMain2Binding.inflate(getLayoutInflater());
+        EdgeToEdge.enable(this);
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        replaceFragment(new HomeFragment());
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.calendar:
+                    replaceFragment(new CalendarFragment());
+                    break;
+
+
+            }
+
+            return true;
+        });
+    }
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
+    }
+}
